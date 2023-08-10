bc817150233744f7491a2d6ee9f1496b0118ca36
patule54 <136695721+patule54@users.noreply.github.com>
2023-08-10 17:38:46 +0500

    Delete advertisment directory

--- advertisment/app_advertisment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Advertisement
-
-class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description','price', 'created_at','auction']
-    list_filter = ['auction', 'created_at']
-
-admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisment/app_advertisment/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(reqests):
-    return render(reqests, 'index.html')
-
-def top_sellers(request):
-    return render(request, 'top-sellers.html')
