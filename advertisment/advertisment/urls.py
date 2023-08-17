2c51d6d27d17104f1f3d33481f24ed6eca62176a
patule54 <136695721+patule54@users.noreply.github.com>
2023-08-17 17:28:46 +0500

    Delete advertisment directory

--- advertisment/advertisment/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app_advertisment.urls')),
-    path('lesson_4', include('app_lesson_4.urls')),
-]
